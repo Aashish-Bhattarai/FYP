33a1f6641a296b8e0133a122b7db6b773654325b
Aashish Bhattarai <bhattaraiaashish100@gmail.com>
2024-02-19 14:11:34 +0545

    Second commit

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -8,7 +8,8 @@ const NavBar = () => {
             <ul className="nav-list">
                 <li className="nav-item">Home</li>
                 <li className="nav-item">Services</li>
-                <li className="nav-item">About</li>
+                {/* <li className="nav-item">About</li> */}
+                
             </ul>
         </nav>
     );
